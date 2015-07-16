eb77a2e2252afae964e6e7348eeea599e3f33706
Khemmachart <nueve.sr@gmail.com>
2015-07-16 19:08:44 +0700

    objc version

---
+++ Podfile
@@ -0,0 +1,4 @@
+pod 'FBSDKCoreKit'
+pod 'FBSDKLoginKit'
+pod 'FBSDKShareKit'
+pod 'Google/Analytics', '~> 1.0.0'
---
+++ whyppllikeyou/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  whyppllikeyou
+//
+//  Created by KHUN NINE on 7/16/15.
+//  Copyright (c) 2015 KHUN NINE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+@interface ViewController : UIViewController <FBSDKLoginButtonDelegate>
+
+
+@end
+
